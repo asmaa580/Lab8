c2a648bea8b2c5ff3136350cbe0f9f949e44ab0a
habeeba148 <es.habiba.shady2023@alexu.edu.eg>
2025-11-21 19:20:06 +0200

    creating Question class

---
+++ src/main/java/com/mycompany/lab8/Question.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab8;
+
+/**
+ *
+ * @author u s e r
+ */
+public class Question {
+    
+}
